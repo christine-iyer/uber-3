8141c28a0b55c6f6b819641b258ade14aa472937
chrisIyer <hellohotbox420@gmail.com>
2025-02-19 15:46:48 -0500

    splash screen was working

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,35 @@
 import { ScreenContent } from 'app/components/ScreenContent';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import '../global.css';
+import { useEffect } from 'react';
+import { View, Text } from 'react-native';
 
 export default function Home() {
+  const [fontsLoaded] = useFonts({
+    'PlusJakartaSans-Bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        // Any async operations you need, like fetching data or loading fonts
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        if (fontsLoaded) {
+          await SplashScreen.hideAsync();
+        }
+      }
+    }
+
+    prepare();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <>
       <ScreenContent title="Home" path="App.tsx" />
