eefb899f3eb1818841f6e40785284964e48ee15c
chrisIyer <hellohotbox420@gmail.com>
2025-03-09 15:20:45 -0400

    verified fonts.

--- app/(root)/(tabs)/chat.tsx
+++ app/(root)/(tabs)/chat.tsx
@@ -1,15 +1,17 @@
 import { View, Text, StyleSheet } from 'react-native';
-
+import { typography } from 'styles/typography';
 export default function ChatScreen() {
   return (
     <View style={styles.chat}>
-      <Text>Can yoiu talk?</Text>
+      <Text style={styles.text}>Can yoiu talk?</Text>
     </View>
   );
 }
 const styles = StyleSheet.create({
   chat: {
-    backgroundColor: 'green',
-    fontFamily: '../',
+    backgroundColor: 'antiquewhite',
+  },
+  text: {
+    fontFamily: typography.JakartaExtraLight,
   },
 });
