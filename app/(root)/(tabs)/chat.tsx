636d7264cb62782684892e388daed89177378bf7
chrisIyer <hellohotbox420@gmail.com>
2025-03-10 16:46:35 -0400

    chat done

--- app/(root)/(tabs)/chat.tsx
+++ app/(root)/(tabs)/chat.tsx
@@ -1,17 +1,33 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { typography } from 'styles/typography';
+
+import { images } from './../../../constants/index';
 export default function ChatScreen() {
   return (
-    <View style={styles.chat}>
-      <Text style={styles.text}>Can you talk?</Text>
-    </View>
+    <SafeAreaView style={styles.safeview}>
+      <ScrollView style={styles.scrollview}>
+        <Text style={styles.textone}>Can you talk?</Text>
+        <View style={styles.view}>
+          <Image source={images.message} />
+          <Text style={styles.texttwo}>No Messages Yet</Text>
+          <Text style={styles.textthree}>Start a conversation with your friends and family</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
-  chat: {
+  safeview: { flex: 1, padding: 5, backgroundColor: 'white' },
+  scrollview: { flex: 1 },
+  view: {
+    flex: 1,
+    alignSelf: 'flex-start',
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: 'antiquewhite',
   },
-  text: {
-    fontFamily: typography.JakartaExtraLight,
-  },
+  textone: { fontFamily: typography.JakartaExtraBold },
+  textthree: { fontSize: 24, fontFamily: typography.JakartaExtraBold, marginTop: 12 }, //"text-3xl font-JakartaBold mt-3"
+  texttwo: { marginTop: 8, padding: 7, textAlign: 'center', fontSize: 16 }, // "text-base mt-2 text-center px-7"
 });
