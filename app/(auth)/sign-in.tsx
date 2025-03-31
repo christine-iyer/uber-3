27e0870ce5027b95d1c07446a8178978275024ec
chrisIyer <hellohotbox420@gmail.com>
2025-03-31 16:23:39 -0400

    made a pointless change in sign in redo immediately.

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,3 +1,4 @@
+import InputField from 'components/InputField';
 import { useRouter } from 'expo-router';
 import { View, Text, StyleSheet } from 'react-native';
 import { typography } from 'styles/typography';
@@ -6,7 +7,35 @@ import CustomButton from '../../components/CustomButton'; // ✅ Import CustomBu
 
 export default function SignInScreen() {
   const router = useRouter();
+  const { signIn, setActive, isLoaded } = useSignIn();
 
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  const onSignInPress = useCallback(async () => {
+    if (!isLoaded) return;
+
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: form.email,
+        password: form.password,
+      });
+
+      if (signInAttempt.status === 'complete') {
+        await setActive({ session: signInAttempt.createdSessionId });
+        router.replace('/(root)/(tabs)/home');
+      } else {
+        // See https://clerk.com/docs/custom-flows/error-handling for more info on error handling
+        console.log(JSON.stringify(signInAttempt, null, 2));
+        Alert.alert('Error', 'Log in failed. Please try again.');
+      }
+    } catch (err: any) {
+      console.log(JSON.stringify(err, null, 2));
+      Alert.alert('Error', err.errors[0].longMessage);
+    }
+  }, [isLoaded, form]);
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Sign In Here</Text>
